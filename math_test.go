ecabbd1a3ee73c424da4080a72c5c674b76eca6f
adilson1979 <adilsonlustoza@gmail.com>
2023-06-11 10:53:46 -0300

    Sent to remote feature/one 0001

--- math_test.go
+++ math_test.go
@@ -2,16 +2,16 @@ package main
 
 import "testing"
 
-func TestSoma(t *testing.T) {
+func TestCalculo(t *testing.T) {
 
 	soma := soma(15, 15)
-	sub := sub(20, 16)
+	sub := sub(20, 15)
 
 	if soma != 30 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", soma, 30)
 	}
 
 	if sub != 5 {
-		t.Errorf("Não deve passar de jeito nenhum: Resultado %d. Esperado: %d", sub, 5)
+		t.Errorf("It's a new feature %d. Esperado: %d", sub, 5)
 	}
 }
