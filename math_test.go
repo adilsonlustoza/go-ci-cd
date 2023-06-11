d587f05cb2114e7a806bdd82763735a0498a0d36
adilson1979 <adilsonlustoza@gmail.com>
2023-06-11 10:08:41 -0300

    ci: :rocket: Testando PullRequest
    Eu sei que não era para enviar
    
    dura

--- math_test.go
+++ math_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := soma(15, 15)
+	soma := soma(15, 15)
 
-	if total != 30 {
-		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
+	if soma != 30 {
+		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", soma, 30)
 	}
 }
