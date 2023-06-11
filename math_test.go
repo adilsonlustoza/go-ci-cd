beb396d24c78935e2ef077fe7df68d259a2d908e
adilson1979 <adilsonlustoza@gmail.com>
2023-06-11 10:17:36 -0300

    ci: :rocket: Teste PullRequest

--- math_test.go
+++ math_test.go
@@ -6,7 +6,13 @@ func TestSoma(t *testing.T) {
 
 	soma := soma(15, 15)
 
+	sub := sub(20, 16)
+
 	if soma != 30 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", soma, 30)
+	}
+
+	if sub != 5 {
+		t.Errorf("Resultado da sub é inválido: Resultado %d. Esperado: %d", sub, 5)
 	}
 }
